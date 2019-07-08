350d523346713d841399943fc2db4a0b2b779214
Yifan Wang <yfwang@phy.cuhk.edu.hk>
2019-07-08 23:24:21 +0800

    add mg's c code

---
+++ c-code/lalsim-massivegraviton.c
@@ -0,0 +1,39 @@
+#include <lal/LALSimInspiralTestGRParams.h>
+#include <lal/LALStatusMacros.h>
+
+int main(int argc , char *argv[])
+{
+
+	LALDict *nonGRparams = NULL;
+
+	if(nonGRparams==NULL)
+		printf("nonGRparams is a null pointer.\n");
+	else
+		printf("nonGRparams is not a null pointer.\n");
+
+	int cache1 = XLALSimInspiralWaveformParamsNonGRAreDefault(nonGRparams);
+	printf("Create a null nonGR parameter, the nonGR default judgment value is %lf(double float) or %d(int) or (int to double) \n",
+		(double)cache1,cache1);
+	// A direct forced output of cache to %lf shows a wrong value (0.000 rather than 1.000) 
+
+	nonGRparams = XLALCreateDict();
+	printf("After create a structure for the nonGRparams, is it still a null pointer?\n");
+	if(nonGRparams==NULL)
+		printf("Yes\n");
+	else
+		printf("No\n");
+
+	int cache2 = XLALSimInspiralWaveformParamsNonGRAreDefault(nonGRparams);
+	printf("After assigning nonGRparams a structure, the nonGR default judgment value is %d(int) or %lf(double float) \n",
+		cache2,(double)cache2);
+
+	double cache3 = XLALSimInspiralWaveformParamsLookupNonGRDChi0(nonGRparams);
+	printf("The default value of the dchi0 in 'nonGRparams' is %lf.\n",cache3);
+
+	double cache4 = XLALSimInspiralWaveformParamsLookupNonGRMassiveGravitonLambda(nonGRparams);
+	printf("The default value of the MassiveGravitonLambda is %lf.\n", cache4);
+
+	XLALSimInspiralWaveformParamsInsertNonGRMassiveGravitonLambda(nonGRparams,1.e13);
+	double cache5 = XLALSimInspiralWaveformParamsLookupNonGRMassiveGravitonLambda(nonGRparams);
+	printf("The injection value of the MassiveGravitonLambda is %lf.\n", cache5);
+}
--- c-code/make.sh
+++ c-code/make.sh
@@ -1,8 +1,12 @@
-#you include to insert the lallibpath to LD_LIBRARY_PATH by hand
+#you should include the lallibpath to LD_LIBRARY_PATH by hand
 
-lalpath=/home/astro/Gwaves/lscsoft/opt/master/include
-lallibpath=/home/astro/Gwaves/lscsoft/opt/master/lib
-cfile=lalsim-nonGR.c
+#lalpath=/home/astro/Gwaves/lscsoft/opt/master/include
+#lallibpath=/home/astro/Gwaves/lscsoft/opt/master/lib
+#cfile=lalsim-nonGR.c
+lalpath=/home/astro/Gwaves/lscsoft/opt/mylalfork-cor-tgr/include
+lallibpath=/home/astro/Gwaves/lscsoft/opt/mylalfork-cor-tgr/lib
+#LD_LIBRARY_PATH=${lallibpath}:LD_LIBRARY_PATH
+cfile=lalsim-massivegraviton.c
 
 gcc -pthread -I${lalpath} ${cfile} -L${lallibpath} -lm -llal -llalsimulation
 
